97a1e85e70955b5dcd4298280e00457e31f38b5d
NamPhuThuy <ttnam957@gmail.com>
2026-01-02 23:57:11 +0700

    modify: delete something

--- TutorialManager.cs
+++ TutorialManager.cs
@@ -19,86 +19,14 @@ namespace NamPhuThuy.PuzzleTutorial
         {
             get
             {
-                   
                 return _tutorialHand;
             }
             
         }
 
-        private int _currentLevelId = -1;
-        private Coroutine _runningRoutine;
-
-        public bool IsTutorialRunning => _runningRoutine != null;
-
         #region MonoBehaviour Callbacks
-
 
         #endregion
 
-        private void TryStartTutorialForLevel(int levelId)
-        {
-            if (!isEnable) return;
-            if (!data) return;
-
-            var rule = data.GetTutRecord(levelId);
-            if (rule == null) return;
-            if (rule.Steps == null || rule.Steps.Count == 0) return;
-
-            if (_runningRoutine != null)
-            {
-                StopCoroutine(_runningRoutine);
-            }
-
-            _runningRoutine = StartCoroutine(RunTutorialRoutine(levelId, rule));
-        }
-
-        private IEnumerator RunTutorialRoutine(int levelId, TutorialRecord rule)
-        {
-            if (!isEnable) yield break;
-            MMEventManager.TriggerEvent(new ETutorialActivate(levelId));
-
-            for (int i = 0; i < rule.Steps.Count; i++)
-            {
-                var step = rule.Steps[i];
-
-                if (step.DelayBefore > 0f)
-                {
-                    yield return new WaitForSeconds(step.DelayBefore);
-                }
-
-                MMEventManager.TriggerEvent(new ETutorialStepStarted(levelId, step, i));
-
-                if (step.AutoCompleteAfter > 0f)
-                {
-                    yield return new WaitForSeconds(step.AutoCompleteAfter);
-                    MMEventManager.TriggerEvent(new ETutorialStepCompleted(levelId, step, i));
-                }
-                else
-                {
-                    // Wait until some external code calls CompleteStep(...)
-                    bool completed = false;
-                    void OnComplete(ETutorialStepCompleted s)
-                    {
-                        if (s.LevelId == levelId && s.StepIndex == i)
-                            completed = true;
-                    }
-
-                    // In a real implementation you'd register a listener; for brevity we poll a flag
-                    while (!completed)
-                        yield return null;
-                }
-            }
-
-            MMEventManager.TriggerEvent(new ETutorialFinished(levelId));
-            _runningRoutine = null;
-        }
-
-        // Public API for other modules to progress tutorial
-        public void CompleteStep(int levelId, int stepIndex, TutorialStepRecord stepRecord)
-        {
-            if (!isEnable) return;
-            if (!IsTutorialRunning) return;
-            MMEventManager.TriggerEvent(new ETutorialStepCompleted(levelId, stepRecord, stepIndex));
-        }
     }
 }
